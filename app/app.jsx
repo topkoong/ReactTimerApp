908ff3e16688564af0b34d4ffce05889f821f3dd
topkoong <engineer_top@msn.com>
2017-03-29 16:57:59 -0400

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+//This is exactly the same below, creating new variable Route = require('react-router').Route;
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');// all the properties we wanna pull off
+var Main = require('Main');
+
+
+// loading CSS (foundation)
+require('style!css!foundation-sites/dist/css/foundation.min.css'); // css!, meaning we use css loader that we installed
+// we still need to inject css to html so the style actually shows up by adding style loader first.
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles');
+
+
+
+// var obj = {
+//   name: 'Top'
+// };
+// If I wanna pull out the name, I can use destructuring like this.
+//var {name} = obj; // This creates a new variable called name and sets it to equal to whatever obj.name equals to.
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
---
+++ app/components/Navigation.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer App
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/countdown" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">Created by <a href="https://github.com/topkoong/" target="_blank">Theerut Foongkiatcharoen</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Navigation;
