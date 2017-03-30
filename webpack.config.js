e55c80edb5f1df3616cf463cab1055ee0a5302cd
topkoong <engineer_top@msn.com>
2017-03-30 01:05:00 -0400

    Create clock component

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,8 @@ module.exports = {
       Navigation: 'app/components/Navigation.jsx',
       applicationStyles: 'app/styles/app.scss',
       Timer: 'app/components/Timer.jsx',
-      Countdown: 'app/components/Countdown.jsx'
+      Countdown: 'app/components/Countdown.jsx',
+      Clock: 'app/components/Clock.jsx'
     },
     // list of file extensions we wanna process
     extensions: ['', '.js', '.jsx']
