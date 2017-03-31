7ec778917f37a8e7dda81d28a6c7844403752ba0
topkoong <engineer_top@msn.com>
2017-03-31 00:27:25 -0400

    Add static controls component

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,8 @@ module.exports = {
       Timer: 'app/components/Timer.jsx',
       Countdown: 'app/components/Countdown.jsx',
       Clock: 'app/components/Clock.jsx',
-      CountdownForm: 'app/components/CountdownForm.jsx'
+      CountdownForm: 'app/components/CountdownForm.jsx',
+      Controls: 'app/components/Controls.jsx'
     },
     // list of file extensions we wanna process
     extensions: ['', '.js', '.jsx']
