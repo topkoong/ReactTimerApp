f25077a0f26dfc27c6d75b26d6fd1f1d8db3a318
topkoong <engineer_top@msn.com>
2017-03-31 22:43:18 -0400

    Wire up controls component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var Clock = require('Clock');
 var CountdownForm = require('CountdownForm');
+var Controls = require('Controls');
 
 var Countdown = React.createClass({
   getInitialState: function () {
@@ -15,6 +16,12 @@ var Countdown = React.createClass({
         case 'started':
           this.startTimer();
           break;
+        case 'stopped': // reset the clock
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer); // clear the timer
+          this.timer = undefined;
+          break;
       }
     }
   },
@@ -24,9 +31,6 @@ var Countdown = React.createClass({
       this.setState({
         count: newCount >= 0 ? newCount : 0
       });
-
-
-
     }, 1000);
   },
   handleSetCountdown: function (seconds) { // maintain state, and interact with CountdownForm component
@@ -35,13 +39,23 @@ var Countdown = React.createClass({
       countdownStatus: 'started'
     });
   },
+  handleStatusChange: function (newStatus) {
+    this.setState({countdownStatus: newStatus});
+  },
   // when the child calls onSetCountdown from CountdownForm.jsx, it will call handleSetCountdown in the parent function because we define them.
   render: function () {
-    var {count} = this.state;
+    var {count, countdownStatus} = this.state;
+    var renderControlArea = () => {
+      if (countdownStatus !== 'stopped') {
+        return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>;
+      } else {
+        return <CountdownForm onSetCountdown={this.handleSetCountdown}/>;
+      }
+    };
     return (
       <div>
         <Clock totalSeconds={count}/>
-        <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+        {renderControlArea()}
       </div>
     );
   }
